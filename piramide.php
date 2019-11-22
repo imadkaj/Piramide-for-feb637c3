04068233e68b4380e8d4aa4acc68062a1475e9f9
FIRST_Imad LAST_Kajdouh <2058092@talnet.nl>
2019-11-22 09:22:42 +0100

    Uitwerking van de exercise

---
+++ piramide.php
@@ -0,0 +1,14 @@
+<?php
+echo "hoeveel stapeltjes wil je hebben \n";
+$b = readline();
+
+for ($a = 1; $a < $b + 1 ; $a++) {
+    for ($j = 1; $j <= $a; $j++)
+    
+
+    echo"*";
+    echo "\n";
+
+}
+
+
